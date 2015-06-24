291009a40ca622f5e8111c648341e859e336f15e
Safiq M <safiq.m@freshdesk.com>
2015-06-24 16:20:16 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roommate::Application.config.secret_token = 'f11df29d0debd053bdd04f7861288ae77415af768829a460220e7a9bb6b6c1e47dc0346d6f4c0b35649c01cb52b2617fc11276df834b6670d7d067fdaebde25a'
